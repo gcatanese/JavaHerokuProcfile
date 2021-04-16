3d9bd536ae81327699fe3c5248c76be7d653263e
gcatanese <gcatanese@yahoo.com>
2021-04-16 11:31:05 +0200

    Add local lib

--- src/main/java/com/perosa/App.java
+++ src/main/java/com/perosa/App.java
@@ -2,12 +2,16 @@ package com.perosa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.perosa.core.SimpleLib;
+
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+
+        System.out.println("Version->" + SimpleLib.getVersion());
     }
 
 }
